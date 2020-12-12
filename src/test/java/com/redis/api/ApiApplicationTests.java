9bb272302a28c280ecfbaa1156ff6a189d5e1fbe
wangyinhui <13037651897@163.com>
2020-12-12 20:41:12 +0800

    redis_api

---
+++ src/test/java/com/redis/api/ApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.redis.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class ApiApplicationTests {
+
+		@Test void contextLoads( ) {
+		}
+
+}
