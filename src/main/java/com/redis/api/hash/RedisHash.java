70319b32bc09bc4d95004bd53b35cbf69c3e0b75
wangyinhui <13037651897@163.com>
2020-12-19 18:55:19 +0800

    redis_api hash

--- src/main/java/com/redis/api/hash/RedisHash.java
+++ src/main/java/com/redis/api/hash/RedisHash.java
@@ -1,9 +1,13 @@
 package com.redis.api.hash;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.redis.api.pojo.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.hash.Jackson2HashMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 /**
  * @author wangyh
@@ -11,7 +15,28 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class RedisHash {
+		@Autowired
+		private ObjectMapper objectMapper;
 
 		@Autowired private RedisTemplate<String, Object> redisTemplate;
+
+		public void redisHash() {
+				Person person = new Person();
+				person.setName("Tom");
+				person.setAge(10);
+				String key = "tom";
+				Jackson2HashMapper jm = new Jackson2HashMapper(objectMapper, false);
+				redisTemplate.opsForHash().putAll(key, jm.toHash(person));
+				Map<Object, Object> entries = redisTemplate.opsForHash( ).entries( key );
+				entries.entrySet().stream().forEach(kv -> {
+						System.out.println(kv.getKey() + ":" + kv.getValue());
+				});
+		}
+
+
+
+
+
+
 
 }
